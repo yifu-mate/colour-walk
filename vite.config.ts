643d3db7972faaaaa50d5eac12ea6efbb7f4d861
yifu <yifu1688888@gmail.com>
2026-05-13 13:01:41 +0800

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/colour-walk/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
