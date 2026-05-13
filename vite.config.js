4e9d9ac6a2eaa2d611cba5207d29c8694efd89aa
yifu <yifu1688888@gmail.com>
2026-05-13 13:08:59 +0800

    Fix base path in JS config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 export default defineConfig({
     plugins: [react()],
+    base: '/colour-walk/',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
